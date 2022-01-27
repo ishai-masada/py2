7b7e1d28acaadeee86b2fbaff5077da89120296b
Ishai Masada <imasada@vtcap.org>
2022-01-27 08:15:06 -0500

    updated header and fixed the type errors.
    dict_values and dict_keys needed to be converted to lists using "list()".
    fixed header with tabular format.

--- lucky_7.py
+++ lucky_7.py
@@ -18,12 +18,12 @@ players = {}
 for player in range(1, 5):
 
     total_funds = 0
-    print("""Welcome to Lucky Sevens!
-          If the dice add up to 7, you gain $4.
-          If they don\'t, then you lose $1.
-          """)
-    player_name = input("Type in your name, you fool: ")
-    pot = int(input("Type in the amount of money you are willing to put in the pot: "))
+    header = "%-24s%-36s%-32s" % ("Welcome to Lucky Sevens!\n",
+                       "If the dice add up to 7, you gain $4.\n",
+                       "If they don\'t, then you lose $1.")
+    print(header)
+    player_name = input("\nType in your name, you fool: ")
+    pot = int(input("\nType in the amount of money you are willing to put in the pot: "))
 
     number_of_rolls = 0
 
@@ -38,17 +38,24 @@ for player in range(1, 5):
 
         if total_funds < pot:
             total_funds = pot
+
         number_of_rolls += 1
 
     players[player_name] = number_of_rolls
-    print(f"This is the highest value of the pot in the game: {total_funds}")
-    print(f"This is the number of rolls it took the pot to empty: {number_of_rolls}")
+    print(f"\nThis was the highest value of the pot in the game: {total_funds}")
+    print(f"\nThis is the number of rolls it took the pot to empty: {number_of_rolls}")
+
+most_rolls_name = list(players.keys())[list(players.values()).index(max(players.values()))]
+least_rolls_name = list(players.keys())[list(players.values()).index(min(players.values()))]
 
 print(players)
 # Display the name of the player with the largest amount of rolls
-print(f"""The player that had the largest amount of
-      rolls: {players.keys()[players.values().index(max(players.values()))]}""")
+print(f"""\nThe player that had the largest amount of
+      rolls: {most_rolls_name}
+      This is the number of rolls they had: {players.get(most_rolls_name)}""")
 
 # Display the name of the player with the least amount of rolls
-print(f"""The player that had the least amount of
-      rolls: {players.keys()[players.values().index(min(players.values()))]}""")
+print(f"""\nThe player that had the least amount of
+      rolls: {least_rolls_name}
+      This is the number of rolls they had: {players.get(least_rolls_name)}
+      """)
