8bf1201e8fbbb1fac86c443387b19bf4a56eb733
Ishai Masada <imasada@vtcap.org>
2022-02-14 08:33:00 -0500

    fixed the get_name_cost function
    used try and except

--- wk4/grocery_list.py
+++ wk4/grocery_list.py
@@ -34,7 +34,7 @@ def display():
     print("\n- Type \"Delete\" to  Delete items from the grocery list")
     print("\n- Type \"Print\" to Print the grocery list")
     print("\n- Type \"Go shopping\" to Go shopping")
-    print("\n- Type \"Skip\" to skip to the next week")
+    print("\n- Type \"Next\" to go to the next week")
 
 
 #####################################################################################
@@ -60,6 +60,56 @@ def get_choice():
 
 #####################################################################################
 #
+# Function Name:  get_name_cost
+#
+# Date:           2/7/22
+#
+# Purpose:       Prompts the user for the name of an item and the cost of that item
+#
+# Who called           N/A
+#
+# Functions:             None
+#
+# Update:                 None
+#
+#####################################################################################
+def get_name_cost():
+    items = {}
+    while True:
+        while True:
+            item_name = input("\nType in the name of the item that you want to add to the "
+                               "grocery list, or type nothing to finish: ").strip().lower().title()
+            try:
+                float(item_name)
+                int(item_name)
+                print("\nYou can only enter letters, not numbers")
+                continue
+
+            except:
+                break
+
+        if len(item_name) == 0:
+            break
+
+        while True:
+            cost = input("\nType in the cost of the item or type nothing to cancel: ").strip().lower()
+            try:
+                float(cost)
+                break
+            except:
+                if cost.isnumeric() == False:
+                    print("\nYou can only enter numbers, not letters")
+                    continue
+
+        if len(cost) == 0:
+            break
+
+        items[item_name] = float(cost)
+
+    return items
+
+#####################################################################################
+#
 # Function Name:  add
 #
 # Date:           2/7/22
@@ -73,10 +123,12 @@ def get_choice():
 # Update:                 None
 #
 #####################################################################################
-def add(GROCERY_LIST):
-    item_name = input("Type in the item name to add: ")
-    item_cost = input("Type in the item cost: ")
-    GROCERY_LIST[item_name.title()] = item_cost
+def add(GROCERY_LIST, week_cost):
+    for item_name, cost in get_name_cost().items():
+        GROCERY_LIST[item_name] = float(cost)
+
+    week_cost = sum(GROCERY_LIST.values())
+    return GROCERY_LIST, week_cost
 
 #####################################################################################
 #
@@ -93,9 +145,21 @@ def add(GROCERY_LIST):
 # Update:                 None
 #
 #####################################################################################
-def delete(GROCERY_LIST):
-    item_name = input("Type in the item name to remove: ").strip().lower().title()
-    GROCERY_LIST.pop(item_name)
+def delete(GROCERY_LIST, week_cost):
+    while True:
+        if len(GROCERY_LIST) == 0:
+            print("\nThe grocery list is empty.")
+            break
+
+        item_name = input("Type in the name of the item that you want to delete from the "
+                           "grocery list, or type nothing to finish: ").strip().lower().title()
+        if len(item_name) == 0:
+            break
+
+        GROCERY_LIST.pop(item_name)
+    week_cost = sum(GROCERY_LIST.values())
+
+    return GROCERY_LIST, week_cost
 
 #####################################################################################
 #
@@ -133,22 +197,14 @@ def print_list(GROCERY_LIST):
 # Update:                 None
 #
 #####################################################################################
-def get_list(GROCERY_LIST):
+def get_list(GROCERY_LIST, week_cost):
     GROCERY_LIST.clear()
+    for item_name, cost in get_name_cost().items():
+        GROCERY_LIST[item_name] = float(cost)
 
-    while True:
-        item_name = input("Type in the name of the item that you want to add to the "
-                           "grocery list, or type nothing to finish: ").strip().lower().title()
-        if len(item_name) == 0:
-            break
+    week_cost = sum(GROCERY_LIST.values())
 
-        cost = input("Type in the cost of the item or type nothing to cancel: ").strip().lower()
-        if len(cost) == 0:
-            break
-
-        GROCERY_LIST[item_name] = cost
-    return GROCERY_LIST
-
+    return GROCERY_LIST, week_cost
 
 #####################################################################################
 #
@@ -169,8 +225,8 @@ def go_shopping(GROCERY_LIST, week_cost):
     print('\nItems purchased:\n')
     for item_name in GROCERY_LIST.keys():
         print('-', item_name)
+    week_cost = round(sum(GROCERY_LIST.values()), 3)
     print(f'\nTotal Cost: ${week_cost}')
-    return total_cost
 
 total_cost = 0
 
@@ -179,27 +235,26 @@ GROCERY_LIST = {}
 
 count = 0
 
-for i in range(5):
+for i in range(4):
 
-    GROCERY_LIST = get_list(GROCERY_LIST)
+    week_cost = 0
+    GROCERY_LIST, week_cost = get_list(GROCERY_LIST, week_cost)
 
     # Menu prompt
     while True:
-
-        week_cost = sum(GROCERY_LIST.values())
 
         # Store the user's input into a variable
         choice = get_choice()
 
         # Execute the correct function that corresponds to the user's input
-        if choice == 'skip':
+        if choice == 'next':
             break
 
         elif choice == 'add':
-           add(GROCERY_LIST)
+           GROCERY_LIST, week_cost = add(GROCERY_LIST, week_cost)
 
         elif choice == 'delete':
-           delete(GROCERY_LIST)
+           GROCERY_LIST, week_cost = delete(GROCERY_LIST, week_cost)
 
         elif choice == 'print':
            print_list(GROCERY_LIST)
@@ -211,5 +266,6 @@ for i in range(5):
         else:
             print("\nThat was not any of the options. Try again.")
 
+    total_cost += week_cost
 
 print(f'\nThis is the total amount of money spent over the 4 weeks: {total_cost}')
