37f91d7ad1c8b609efe716ef5b6e379995670e09
Ishai Masada <imasada@vtcap.org>
2022-03-16 09:21:37 -0400

    finished the file

--- wk8/discussion/plots.py
+++ wk8/discussion/plots.py
@@ -1,2 +1,13 @@
 import matplotlib.pyplot as pyplot
 import seaborn
+import numpy
+
+x_values = [num for num in range(7)]
+y_values = [num for num in range(7)]
+
+seaborn.lineplot(x=x_values, y=y_values)
+pyplot.show()
+
+time = numpy.arange(0, 20, 1)
+pyplot.plot(time, numpy.sin(time))
+pyplot.show()
